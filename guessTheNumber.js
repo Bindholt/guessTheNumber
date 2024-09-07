68e4d2e821d3d0e6373a326942b0d302baaf6583
Bindholt <cbindholt@gmail.com>
2024-09-07 10:15:45 +0200

    del 2

--- guessTheNumber.js
+++ guessTheNumber.js
@@ -1,5 +1,14 @@
 window.addEventListener("load", main);
 
+let max = 100;
+let min = 0;
+let guesses = 0;
+let guess;
+
+const guessAmount = {
+
+}
+
 
 function main() {
     document.querySelector("#start-btn").addEventListener("click", start);
@@ -10,18 +19,22 @@ function main() {
 
 function start(){
     document.querySelector("#guesses").style.display = "block";
-
     submitGuess();
 } 
 
 function submitGuess(tooHighBool) {
-    let guess = generateGuess();
+    if (max >= min) {
+        guess = generateGuess();
+        registerGuess(guess, tooHighBool);
+    } else {
+        refuseGuessing();
+    }
 
-    registerGuess(guess, tooHighBool);
 }
 
 function generateGuess() {
-    return Math.floor(Math.random() * 100) + 1;
+    guesses++;
+    return Math.floor((max+min)/2);
 }
 
 function registerGuess(guess, tooHighBool) {
@@ -38,16 +51,49 @@ function registerGuess(guess, tooHighBool) {
     document.querySelector("#guess").innerHTML = guess;
 }
 
+function refuseGuessing() {
+    document.querySelector("#latest-guess").insertAdjacentHTML("beforebegin", /*html*/ `
+        <li>
+            You're lying! There are no more numbers to guess! The answer is ${guess}.
+        </li>    
+    `)
+    document.querySelector("#btn-span").style.display = "none";
+    correct();
+}
+
 function tooHigh(){
+    max = guess - 1;  
+    console.log(max);
     submitGuess(true);
 }
 
 function tooLow(){
+    min = guess + 1;
+    console.log(min);
     submitGuess(false);
 }
 
 function correct() {
+    let successMsg;
+    if (guesses == 1) {
+        successMsg = "FIRST TRY! EASY!";
+    } else if (guesses < 4) {
+        successMsg = "That wasn't the hardest number to guess now was it?";
+    } else if (guesses < 6) {
+        successMsg = "Decent job, but I've seen better!";
+    } else if (guesses < 8) {
+        successMsg = "Okay that was a hard one, but I got it!";
+    }
+
+    if(guess == 42) {
+        successMsg = "Frick off, Peter";
+    }
+
     document.querySelector("#btn-span").style.display = "none";
     document.querySelector("#success-span").style.display = "inline";
-    document.querySelector("#success").style.display = "block";
+    document.querySelector("#success").insertAdjacentHTML("beforebegin", /*html*/ `
+        <li>
+            ${successMsg}
+        </li>    
+    `)
 }
